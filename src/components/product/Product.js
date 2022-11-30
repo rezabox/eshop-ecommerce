92d53ccc07d0e784ee7f90d1ce12bc362f01308b
rezabox <reza.asareh81@gmail.com>
2022-11-30 22:44:33 +0330

    get Default Middleware

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,9 +1,25 @@
 import React from "react";
+import useFetchCollection from "../../customHooks/useFetchCollection";
 import styles from "./Product.module.scss";
 import ProductFilter from "./productFilter/ProductFilter";
 import ProductList from "./productList/ProductList";
-
+import {
+  selectProducts,
+  STORE_PRODUCTS,
+} from "../../redux/slice/productSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 const Product = () => {
+ const  { data, isLoading } = useFetchCollection('product');
+ const product = useSelector(selectProducts);
+ const dispatch = useDispatch();
+ useEffect(()=>{
+    dispatch(
+      STORE_PRODUCTS({
+        product:data,
+      })
+    )
+ },[dispatch,data])
   return (
     <section>
       <div className={`container ${styles.product}`}>
@@ -11,7 +27,7 @@ const Product = () => {
           <ProductFilter />
         </aside>
         <div className={styles.content}>
-          <ProductList />
+          <ProductList product={product} />
         </div>
       </div>
     </section>
--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,9 +1,38 @@
 import React from "react";
-
+import { useState } from "react";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+import styles from "./ProductList.module.scss";
 const ProductList = () => {
+  const [grid, setGrid] = useState(true);
   return (
-    <div>
-      <h2>Product List.</h2>
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icon}>
+          <BsFillGridFill
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
+          <FaListAlt size={23} color="#0066d4" onClick={() => setGrid(false)} />
+          <p>
+            <b>4</b> Products found
+          </p>
+        </div>
+        <div>
+          <p>Search</p>
+        </div>
+        <div className={styles.sort}>
+          <label>Sort by</label>
+          <select>
+            <option value="Latest">Latest</option>
+            <option value="Lowest Price">Lowest Price</option>
+            <option value="Highest Price">Highest Price</option>
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
